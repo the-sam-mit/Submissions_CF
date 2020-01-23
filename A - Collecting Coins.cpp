273857f1ff0e257f468e396b47016a1f70f25729
Samarth Mittal <samarthmittal94@gmail.com>
2020-01-23 20:47:27 +0530

    Create A - Collecting Coins.cpp

---
+++ A - Collecting Coins.cpp
@@ -0,0 +1,61 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include<bits/stdc++.h>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long int
+#define ull unsigned long long int
+#define ff first 
+#define ss second
+#define pb(x) push_back(x)
+#define pf(x) push_front(x)
+#define start_routine() int begtime = clock();
+#define end_routine() int endtime = clock(); \
+                      cerr << "\n\n" << "Time elapsed: " << \
+                      (endtime - begtime)*1000/CLOCKS_PER_SEC << " ms\n\n"; \
+                      return 0
+#define testcase(t) int t;cin>>t;while(t--)
+#define mp(x,y) make_pair(x,y)
+#define in(x) insert(x)
+#define rep(i,a,b)  for (__typeof((b)) i=(a);i<=(b);i++)
+#define nrep(i,a,b)  for (__typeof((b)) i=(b);i>=(a);i--)
+#define PI 3.14159265358979323846
+#define SP(x) setprecision(x)
+#define reflex ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define M 1000000007
+#define sz(a) (int)a.size()
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define UNIQUE(x) x.resize(distance(x.begin(), unique(all(x))))
+#define mem(arr,val) memset(arr,val,sizeof(arr)) //For "0" and "-1"                  
+#define inp0(a,n) rep(i,0,n-1) cin>>a[i];
+#define inp1(a,n) rep(i,1,n) cin>>a[i]; 
+#define prll(x) printf("%lld",x)
+#define nth_ele(v,n) nth_element(v.begin(), v.begin()+n ,v.end())//O(N)
+#define DEBUG(x) cerr << (#x) << ": " << (x) << endl
+#define DEBUGV(x) for(auto IT:x) cout<<IT<<" "; cout<<endl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tree< int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+ll gcd(ll n1, ll n2){if(n2!=0) return gcd(n2, n1%n2);else return n1;}
+ll powerM(ll a,ll b){if(b==0) return 1;  if(b==1) return a%M; ll x=powerM(a,b/2)%M; if(b&1){return ((((a%M)*(x%M))%M)*(x%M))%M;}return ((x%M)*(x%M))%M;}
+ll power(ll a,ll b){if(b==0) return 1;  if(b==1) return a; ll x=power(a,b/2); if(b&1){return ((((a)*(x)))*(x));}return ((x)*(x));}
+ 
+const ll INF=1e18; 
+const int SIZE = 300005;
+
+int main(){
+	testcase(T){
+		ll a,b,c,n;
+		cin>>a>>b>>c>>n;
+		ll A  = (n+b+c-(2LL*a))/3LL;
+		ll B = a + A - b;
+		ll C = a + A - c;
+		ll AA=((n+b+c-(2LL*a))/3LL)+1LL;
+		ll BB = a + AA - b;
+		ll CC = a + AA - c;
+		// cout<<A<<" "<<Bz
+		if((A+B+C==n && A>=0 && B>=0 && C>=0) || (AA+BB+CC==n && AA>=0 && BB>=0 && CC>=0)) cout<<"YES\n";
+		else cout<<"NO\n";
+	}	 
+}
